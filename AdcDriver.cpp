31ff62c470057b12b362c48dfd87f744dee42a2f
Thaianenaiara <135857494+Thaianenaiara@users.noreply.github.com>
2024-06-26 10:32:01 -0400

    Create AdcDriver.cpp

---
+++ AdcDriver.cpp
@@ -0,0 +1,24 @@
+#include "test\driver\AdcDriverTest.h"
+
+#include <Arduino.h>
+#include <unity.h>
+
+#include "AdcDriver.h"
+#include "port/HeltecV3Board.h"
+
+void setUp(void) {}
+
+void tearDown(void) {}
+
+static void TestAdc(void) {
+  Status_t result = AdcDriverInit();
+  TEST_ASSERT_EQUAL(kSuccess, result);
+  result = AdcDriverReadBattery();
+  TEST_ASSERT_EQUAL(kErrorAdc, result);
+}
+
+void AdcDriverTest(void) {
+  printf("<--------------- ADC DRIVER TESTS - START --------------->\r\n");
+  RUN_TEST(TestAdc);
+  printf("<------------------- ADC DRIVER - END ------------------->\r\n");
+}
